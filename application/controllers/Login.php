3aa14a4beb12fadc25b66cec49524ac229951f20
affantampubolon <affan.tampubolon@gmail.com>
2021-01-13 16:30:53 +0700

    firstcommit

---
+++ application/controllers/Login.php
@@ -0,0 +1,45 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model("User_model");
+        $this->load->library('form_validation');
+    }
+    
+     public function index()
+	{
+        // jika form login disubmit
+        if($this->input->post()){
+            if($this->user_model->doLogin()) redirect(site_url('welcome'));
+        }
+
+        // tampilkan halaman login
+		$this->load->view('login');
+    }
+    
+    public function logout()
+    {
+        // hancurkan semua sesi
+        $this->session->sess_destroy();
+        redirect(site_url('login'));
+    }
+}
