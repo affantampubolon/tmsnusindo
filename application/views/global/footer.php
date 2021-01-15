fb16365ce972e295a110c5f39cd89711de1a729a
affantampubolon <affan.tampubolon@gmail.com>
2021-01-15 15:41:35 +0700

    login controller

--- application/views/global/footer.php
+++ application/views/global/footer.php
@@ -1,6 +1,6 @@
 <!-- Main Footer -->
 <footer class="main-footer">
-    <strong>Copyright <?php echo SITE_NAME ." ". Date('Y') ?></strong>
+    <strong>Copyright </strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 1.0.0-rc
