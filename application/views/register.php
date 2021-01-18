bbe4082810c629ac46dac753720cd9b9a22a3d02
affantampubolon <affan.tampubolon@gmail.com>
2021-01-18 15:16:01 +0700

    login controller

--- application/views/register.php
+++ application/views/register.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>TMS | Log in</title>
+  <title>TMS Nusindo | Registration</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -15,66 +14,43 @@
   <!-- Theme style -->
   <link rel="stylesheet" href="<?php echo base_url(''); ?>assets/dist/css/adminlte.min.css">
 </head>
-<body class="hold-transition login-page dark-mode">
-<div class="login-box">
-  <!-- /.login-logo -->
+<body class="hold-transition register-page dark-mode">
+<div class="register-box">
   <div class="card card-outline card-primary">
     <div class="card-header text-center">
       <h1><b>TMS</b>Nusindo</h1>
     </div>
     <div class="card-body">
-      <p class="login-box-msg">Sign in to start your session</p>
+      <p class="login-box-msg">Choose Your Profile</p>
 
-      <form action="<?= site_url('login/login') ?>" method="POST">
+      <form action="" method="post">
         <div class="input-group mb-3">
-          <input type="username" name="username" class="form-control" placeholder="Username">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
-            </div>
-          </div>
+          <button type="submit" action="<?php echo base_url(''); ?>index.php/Reguser/register" class="btn btn-primary btn-block">Marketing</button>
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
+          <button type="submit" class="btn btn-primary btn-block">Technician</button>
+        </div>
+        <div class="input-group mb-3">
+          <button type="submit" class="btn btn-primary btn-block">Customer</button>
         </div>
         <div class="row">
+          <!-- /.col -->
           <div class="col-8">
-            <div class="icheck-primary">
-              <input type="checkbox" id="remember">
-              <label for="remember">
-                Remember Me
-              </label>
-            </div>
+            <a href="<?php echo base_url(); ?>index.php/login" class="text-center">Back</a>
           </div>
           <!-- /.col -->
+          <!-- /.col -->
           <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block" value="Login">Sign In</button>
+            
           </div>
           <!-- /.col -->
         </div>
       </form>
-
-      <div class="social-auth-links text-center mt-2 mb-3">
-      </div>
-      <!-- /.social-auth-links -->
-
-      <p class="mb-1">
-        <a href="forgot-password.html">I forgot my password</a>
-      </p>
-      <p class="mb-0">
-        <a href="<?php echo base_url(); ?>index.php/Reguser" class="text-center">Register a new membership</a>
-      </p>
     </div>
-    <!-- /.card-body -->
-  </div>
-  <!-- /.card -->
+    <!-- /.form-box -->
+  </div><!-- /.card -->
 </div>
-<!-- /.login-box -->
+<!-- /.register-box -->
 
 <!-- jQuery -->
 <script src="<?php echo base_url(''); ?>assets/plugins/jquery/jquery.min.js"></script>
